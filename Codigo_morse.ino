bbb00cdea2157c63c4918dcba8867909ee883594
JuLiA1o1 <144991153+JuLiA1o1@users.noreply.github.com>
2023-09-25 21:37:15 +0200

    Add files via upload

---
+++ Codigo_morse.ino
@@ -0,0 +1,76 @@
+int dotDuration = 500;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LED_BUILTIN, OUTPUT);  
+}
+
+void loop() {
+  // J
+  digitalWrite(13, 1);
+  delay(dotDuration);
+  digitalWrite(13, 0);
+  delay(dotDuration);
+  digitalWrite(13, 1);
+  delay(dotDuration*3);
+  digitalWrite(13, 0);
+  delay(dotDuration);
+  digitalWrite(13, 1);
+  delay(dotDuration*3);
+  digitalWrite(13, 0);
+  delay(dotDuration);
+  digitalWrite(13, 1);
+  delay(dotDuration*3);
+  digitalWrite(13, 0);
+  delay(dotDuration*3);
+  // U
+  digitalWrite(13, 1);
+  delay(dotDuration);
+  digitalWrite(13, 0);
+  delay(dotDuration);
+  digitalWrite(13, 1);
+  delay(dotDuration);
+  digitalWrite(13, 0);
+  delay(dotDuration);
+  digitalWrite(13, 1);
+  delay(dotDuration*3);
+  digitalWrite(13, 0);
+  delay(dotDuration*3);
+  // L
+  digitalWrite(13, 1);
+  delay(dotDuration);
+  digitalWrite(13, 0);
+  delay(dotDuration);
+  digitalWrite(13, 1);
+  delay(dotDuration*3);
+  digitalWrite(13, 0);
+  delay(dotDuration);
+  digitalWrite(13, 1);
+  delay(dotDuration);
+  digitalWrite(13, 0);
+  delay(dotDuration);
+  digitalWrite(13, 1);
+  delay(dotDuration);
+  digitalWrite(13, 0);
+  delay(dotDuration*3);
+  // I
+  digitalWrite(13, 1);
+  delay(dotDuration);
+  digitalWrite(13, 0);
+  delay(dotDuration);
+  digitalWrite(13, 1);
+  delay(dotDuration);
+  digitalWrite(13, 0);
+  delay(dotDuration*3);
+  // A
+  digitalWrite(13, 1);
+  delay(dotDuration);
+  digitalWrite(13, 0);
+  delay(dotDuration);
+  digitalWrite(13, 1);
+  delay(dotDuration*3);
+  digitalWrite(13, 0);
+  delay(dotDuration*5);
+  // space
+
+}
